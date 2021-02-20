71b6e9a9b9c283ebf074e58bbab33235b4abef08
jordanchan <jordancmchan@hotmail.com>
2021-02-20 15:32:41 -0500

    added some base html and pushing working api

--- app.js
+++ app.js
@@ -1,38 +1,35 @@
 const plant = {}
-
-
-plant.apiUrl = 'https://trefle.io/api/v1/plants'; 
+const proxy = 'http://proxy.hackeryou.com';
+plant.apiUrl = 'https://trefle.io/api/v1/plants';
 plant.apiKey = '8Wqyv65CkcZFWu8BYoOZfiYhnd9MmCLE3d6t-loWYd0';
-
 // get image from api 
 
-plant.getImages = () =>{
 
-     const url = new URL(plant.apiUrl);
-     url.search = new URLSearchParams({
-          token: plant.apiKey,
-     });
-
-     fetch(url).then((response) => {
-          return response.json()
-     }).then((jsonResponse)=>{
-          console.log(jsonResponse)
-     });
-
-
+plant.getImages = () => {
+    const url = new URL(proxy);
+    url.search = new URLSearchParams({
+        reqUrl: plant.apiUrl,
+        'params[token]': plant.apiKey
+    });
+    fetch(url).then((response) => {
+        return response.json()
+    }).then((jsonResponse) => {
+        console.log(jsonResponse)
+     //plant.displayImages(jsonResponse);
+    });
 };
 
 
+plant.displayImages = (apiData) =>{
+
+
+}
 
 
 
 
 
-
-
-plant.init = () =>{
-     plant.getImages();
-
+plant.init = () => {
+    plant.getImages();
 };
-
-plant.init(); 
+plant.init();
