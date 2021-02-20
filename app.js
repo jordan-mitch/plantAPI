e27001e0d014ab67172b98f7a3eb094802ea7eae
mitch <jmitchelreed@gmail.com>
2021-02-20 00:48:33 -0500

    got an object to return from endpoint plants, but only the same 20 no matter what page number

--- app.js
+++ app.js
@@ -1,34 +1,29 @@
+
 const plant = {}
 
 
-plant.apiUrl = 'https://trefle.io/api/v1/plants'; 
+plant.apiUrl = `https://trefle.io/api/v1/plants?page=1`; 
 plant.apiKey = '8Wqyv65CkcZFWu8BYoOZfiYhnd9MmCLE3d6t-loWYd0';
 
-// get image from api 
+
 
 plant.getImages = () =>{
 
-     const url = new URL(plant.apiUrl);
+
+     const url = new URL(`https://cors-anywhere.herokuapp.com/${plant.apiUrl}`);
      url.search = new URLSearchParams({
           token: plant.apiKey,
      });
 
-     fetch(url).then((response) => {
-          return response.json()
-     }).then((jsonResponse)=>{
+     fetch(url)
+     .then((response) => {
+          return response.json();
+     })
+     .then((jsonResponse)=>{
           console.log(jsonResponse)
      });
 
-
 };
-
-
-
-
-
-
-
-
 
 plant.init = () =>{
      plant.getImages();
