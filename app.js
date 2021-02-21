a5715e6f1529a3c26f291f997534eab26b9ac08b
mitch <jmitchelreed@gmail.com>
2021-02-20 20:08:20 -0500

    initialized branch

--- app.js
+++ app.js
@@ -1,33 +1,49 @@
 
 const plant = {}
+const proxy = 'http://proxy.hackeryou.com';
+plant.apiUrl = 'https://trefle.io/api/v1/plants';
+plant.apiKey = '8Wqyv65CkcZFWu8BYoOZfiYhnd9MmCLE3d6t-loWYd0';
 
 
-plant.apiUrl = `https://trefle.io/api/v1/plants?page=1`; 
-plant.apiKey = `8Wqyv65CkcZFWu8BYoOZfiYhnd9MmCLE3d6t-loWYd0`;
+plant.getImages = () => {
 
+     const randomNum = Math.floor((Math.random() * 18879) + 1);
+     const url = new URL(proxy);
+    url.search = new URLSearchParams({
+        reqUrl: plant.apiUrl,
+        'params[token]': plant.apiKey,
+        'params[page]': randomNum, // <------ check that out. I dont think its the "right" way to do this but its working!
+    });
 
-
-plant.getImages = () =>{
-
-
-     const url = new URL(`https://cors-anywhere.herokuapp.com/${plant.apiUrl}`);
-     url.search = new URLSearchParams({
-          token: plant.apiKey,
-     });
-
-     fetch(url)
-     .then((response) => {
-          return response.json();
-     })
-     .then((jsonResponse)=>{
-          console.log(jsonResponse)
-     });
-
+    fetch(url).then((response) => {
+        return response.json()
+    }).then((jsonResponse) => {
+     //    console.log(jsonResponse)
+     plant.displayImage(jsonResponse);
+    });
 };
 
-plant.init = () =>{
+
+plant.displayImage = (apiData) =>{
+     
+     const apiImages = apiData.data; 
+     let randomImage = apiImages[Math.floor((Math.random() * apiImages.length ))];
+     
+     const divElement = document.querySelector('#imageContainer');
+     const image = document.createElement('img');
+
+     image.src = randomImage.image_url
+     image.alt = randomImage.scientific_name; 
+
+     divElement.appendChild(image)
+}
+
+// for the display image function, i decided to display one photo at a time but we can fix that if we change our minds. Unfortunately, not every single plant comes with an image_url so we might need to change some of our ideas? but i am able to display the image onto the screen. 
+
+
+
+plant.init = () => {
      plant.getImages();
 
 };
-
-plant.init(); 
+plant.init();
