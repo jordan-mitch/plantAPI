2759bcd82d082d7b449c1eeafbf630922a1351d5
mitch <jmitchelreed@gmail.com>
2021-02-21 21:32:51 -0500

    worked on filtering

--- app.js
+++ app.js
@@ -1,49 +1,65 @@
 
 const plant = {}
+
 const proxy = 'http://proxy.hackeryou.com';
-plant.apiUrl = 'https://trefle.io/api/v1/plants';
+plant.apiUrl = 'https://trefle.io/api/v1/species';
 plant.apiKey = '8Wqyv65CkcZFWu8BYoOZfiYhnd9MmCLE3d6t-loWYd0';
 
 
-plant.getImages = () => {
+plant.getData = () => {
 
-     const randomNum = Math.floor((Math.random() * 18879) + 1);
-     const url = new URL(proxy);
+    const randomNum = Math.floor((Math.random() * 1333) + 1);
+    const url = new URL(proxy);
     url.search = new URLSearchParams({
         reqUrl: plant.apiUrl,
         'params[token]': plant.apiKey,
-        'params[page]': randomNum, // <------ check that out. I dont think its the "right" way to do this but its working!
+        'params[page]': randomNum,
     });
 
     fetch(url).then((response) => {
+
         return response.json()
     }).then((jsonResponse) => {
-     //    console.log(jsonResponse)
-     plant.displayImage(jsonResponse);
+
+        plant.displayData(jsonResponse);
     });
 };
 
 
-plant.displayImage = (apiData) =>{
-     
-     const apiImages = apiData.data; 
-     let randomImage = apiImages[Math.floor((Math.random() * apiImages.length ))];
-     
-     const divElement = document.querySelector('#imageContainer');
-     const image = document.createElement('img');
+plant.displayData = (apiData) => {
 
-     image.src = randomImage.image_url
-     image.alt = randomImage.scientific_name; 
+    const apiPlantArray = apiData.data;
 
-     divElement.appendChild(image)
+    let newArray = [];
+
+    apiPlantArray.forEach((plantObjs) => {
+        if (plantObjs.image_url && plantObjs.common_name !== null) { 
+            newArray.push(plantObjs)
+        }
+    })
+
+    console.log(newArray)
+
+    let randomPlant = newArray[Math.floor((Math.random() * newArray.length))];
+    const divElement = document.querySelector('#imageContainer');
+    const image = document.createElement('img');
+
+    image.src = randomPlant.image_url
+    image.alt = randomPlant.common_name
+
+    divElement.appendChild(image)
+
+
+    console.log('Image: ', image.src)
+    console.log('Name: ', image.alt)
+
+
 }
-
-// for the display image function, i decided to display one photo at a time but we can fix that if we change our minds. Unfortunately, not every single plant comes with an image_url so we might need to change some of our ideas? but i am able to display the image onto the screen. 
 
 
 
 plant.init = () => {
-     plant.getImages();
+    plant.getData();
 
 };
 plant.init();
